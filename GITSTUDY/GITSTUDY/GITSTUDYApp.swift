b656b29f30673478d360de5a38885f02197b1dcb
Yunji Lee <yundlee15@gmail.com>
2024-04-12 14:14:41 +0900

    프로젝트 파일을 만들었습니다.

---
+++ GITSTUDY/GITSTUDY/GITSTUDYApp.swift
@@ -0,0 +1,17 @@
+//
+//  GITSTUDYApp.swift
+//  GITSTUDY
+//
+//  Created by 이윤지 on 4/12/24.
+//
+
+import SwiftUI
+
+@main
+struct GITSTUDYApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
